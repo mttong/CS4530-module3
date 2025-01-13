b569d7f0e88a2a90f6809f74b16374c947fbbdc0
mttong <tong.m@northeastern.edu>
2025-01-13 12:59:25 -0500

    moving folders

--- module03-test-adequacy/src/mocksExample/Calculator.ts
+++
@@ -1,7 +0,0 @@
-export default class Calculator {
-    public add(num1: number, num2: number): number {
-      const result: number = num1 + num2;
-      console.log("The result is: ", result);
-      return result;
-    }
-  }
